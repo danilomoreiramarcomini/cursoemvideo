be25672841392cb5ff07c1460759d5acf8e5014a
Danilo Moreira Marcomini <danilomoreiramarcomini@gmail.com>
2022-07-12 09:52:42 -0300

    PEP 8

--- estruturascompostas/ex072.py
+++ estruturascompostas/ex072.py
@@ -1,8 +1,8 @@
-numbers = ('one',  'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve',
-           'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty')
+numbers = ("one",  "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
+           "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty")
 
 while True:
-    numberInput = int(input('Write a number between 1 and 20:'))
+    numberInput = int(input("Write a number between 1 and 20: "))
     if 0 < numberInput <= 20:
         print(numbers[numberInput - 1])
         break
