dcab87b1607829f7b03fd8be1ca217303aedcccc
Danilo Moreira Marcomini <danilomoreiramarcomini@gmail.com>
2022-07-23 15:57:18 -0300

    PEP8

--- estruturascompostas/ex103.py
+++ estruturascompostas/ex103.py
@@ -1,14 +1,25 @@
 def token(player_name="player undefined", number_of_goals='0'):
-    """ This function returns a simple mensage with name and number goals of player entered
+    print(f"{player_name} make {number_of_goals} goals")
+    """ This function returns a simple message with name and number goals of player entered
     :param player_name: optional name
     :param number_of_goals: optional numbers of goals
-    :return: A simple mensage with player_name and number_of_goals
+    :return: A simple message with player_name and number_of_goals
     """""
-    if number_of_goals.isnumeric():
-        number_of_goals = int(number_of_goals)
-    else:
-        number_of_goals = int(0)
-    print(f"The {player_name} make {number_of_goals} goals")
 
 
-token(str(input("Player: ")).title(), str(input("Number of goals: ")))
+name = str(input("Player: "))
+goals = str(input("Goals: "))
+
+if goals.isnumeric():
+    goals = int(goals)
+else:
+    goals = 0
+
+if name.strip() == "":
+    # noinspection PyTypeChecker
+    token(number_of_goals=goals)
+else:
+    # noinspection PyTypeChecker
+    token(name, goals)
+
+
